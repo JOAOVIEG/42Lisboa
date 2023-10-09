ff4bba1bc504116532f362639c6f06978ff121c4
Joao Pedro Cardoso Viegas <joaocard@c2r10s2.42lisboa.com>
2023-10-09 15:39:06 +0100

    so_long first push

---
+++ so_long/inc/so_long.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaocard <joaocard@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 11:45:37 by joaocard          #+#    #+#             */
+/*   Updated: 2023/10/09 15:21:02 by joaocard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+
+#define SO_LONG_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "../libs/minilibx-linux/mlx.h"
+
+#endif
