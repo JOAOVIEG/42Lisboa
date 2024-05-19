68dece393d1f8317a7a016fae5e94750dd594604
Joao Viegas <joao.c.90.viegas@gmail.com>
2024-05-19 13:50:21 +0100

    Update time.c
    deleted unused function.

--- philo/src/time.c
+++ philo/src/time.c
@@ -6,23 +6,11 @@
 /*   By: joaocard <joaocard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/22 10:50:10 by joaocard          #+#    #+#             */
-/*   Updated: 2024/05/18 10:09:28 by joaocard         ###   ########.fr       */
+/*   Updated: 2024/05/19 12:02:07 by joaocard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
-
-long	get_current_time(size_t start)
-{
-	struct timeval	tv;
-	size_t			current_time;
-
-	if (gettimeofday(&tv, NULL) != 0)
-		printf("Error\n gettimeofday()\n");
-	else
-		current_time = (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
-	return (current_time - start);
-}
 
 void	my_usleep(long time, t_table *table)
 {
